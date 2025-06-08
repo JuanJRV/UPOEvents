1e3047f73b7d2b58ba0125d3d396b6c9e5256e55
JuanJRV <114989115+JuanJRV@users.noreply.github.com>
2025-06-08 22:29:29 +0200

    Add files via upload

---
+++ upoevents.sql
@@ -0,0 +1,260 @@
+﻿CREATE DATABASE upoevents;
+USE upoevents;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `upoevents`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bloqueo`
+--
+
+CREATE TABLE `bloqueo` (
+  `id_bloqueo` int(11) NOT NULL,
+  `bloqueado` varchar(30) NOT NULL,
+  `bloqueador` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asistente`
+--
+
+CREATE TABLE `asistente` (
+  `id_asistente` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `evento` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peticion`
+--
+
+CREATE TABLE `peticion` (
+  `estado` varchar(30) NOT NULL,
+  `id_peticion` int(11) NOT NULL,
+  `evento` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `valoracion`
+--
+
+CREATE TABLE `valoracion` (
+  `id_valoracion` int(11) NOT NULL,
+  `valoracion` int(11) NOT NULL,
+  `evento` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `reporte`
+--
+
+CREATE TABLE `reporte` (
+  `id_reporte` int(11) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `asunto` varchar(250) NOT NULL,
+  `reportador` varchar(30) NOT NULL,
+  `reportado` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `apellidos` varchar(30) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `fecha_nacimiento` date NOT NULL,
+  `clave` varchar(250) NOT NULL,
+  `genero` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
+--
+
+CREATE TABLE `evento` (
+  `fecha` date NOT NULL,
+  `aforo` int(11) NOT NULL,
+  `id_evento` int(11) NOT NULL,
+  `nombre` varchar(30) NOT NULL,
+  `lugar` varchar(30) NOT NULL,
+  `hora` varchar(30) NOT NULL,
+  `usuario` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `bloqueo`
+--
+ALTER TABLE `bloqueo`
+  ADD PRIMARY KEY (`id_bloqueo`),
+  ADD KEY `bloqueo_ibfk_1` (`bloqueado`),
+  ADD KEY `bloqueo_ibfk_2` (`bloqueador`);
+
+--
+-- Indices de la tabla `asistente`
+--
+ALTER TABLE `asistente`
+  ADD PRIMARY KEY (`id_asistente`),
+  ADD KEY `peticion_ibfk_1` (`evento`),
+  ADD KEY `peticion_ibfk_2` (`usuario`);
+
+--
+-- Indices de la tabla `peticion`
+--
+ALTER TABLE `peticion`
+  ADD PRIMARY KEY (`id_peticion`),
+  ADD KEY `peticion_ibfk_1` (`evento`),
+  ADD KEY `peticion_ibfk_2` (`usuario`);
+
+--
+-- Indices de la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  ADD PRIMARY KEY (`id_valoracion`),
+  ADD KEY `valoracion_ibfk_1` (`evento`),
+  ADD KEY `valoracion_ibfk_2` (`usuario`);
+
+--
+-- Indices de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD PRIMARY KEY (`id_reporte`),
+  ADD KEY `reporte_ibfk_1` (`reportador`),
+  ADD KEY `reporte_ibfk_2` (`reportado`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indices de la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD PRIMARY KEY (`id_evento`),
+  ADD KEY `evento_ibfk_1` (`usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `bloqueo`
+--
+ALTER TABLE `bloqueo`
+  MODIFY `id_bloqueo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `asistente`
+--
+ALTER TABLE `asistente`
+  MODIFY `id_asistente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `peticion`
+--
+ALTER TABLE `peticion`
+  MODIFY `id_peticion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  MODIFY `id_valoracion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  MODIFY `id_reporte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `evento`
+--
+ALTER TABLE `evento`
+  MODIFY `id_evento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `bloqueo`
+--
+ALTER TABLE `bloqueo`
+  ADD CONSTRAINT `bloqueo_ibfk_1` FOREIGN KEY (`bloqueado`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bloqueo_ibfk_2` FOREIGN KEY (`bloqueador`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `asistente`
+--
+ALTER TABLE `asistente`
+  ADD CONSTRAINT `asistente_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `asistente_ibfk_2` FOREIGN KEY (`evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `peticion`
+--
+ALTER TABLE `peticion`
+  ADD CONSTRAINT `peticion_ibfk_1` FOREIGN KEY (`evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `peticion_ibfk_2` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `valoracion`
+--
+ALTER TABLE `valoracion`
+  ADD CONSTRAINT `valoracion_ibfk_1` FOREIGN KEY (`evento`) REFERENCES `evento` (`id_evento`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `valoracion_ibfk_2` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `reporte`
+--
+ALTER TABLE `reporte`
+  ADD CONSTRAINT `reporte_ibfk_1` FOREIGN KEY (`reportador`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `reporte_ibfk_2` FOREIGN KEY (`reportado`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `evento`
+--
+ALTER TABLE `evento`
+  ADD CONSTRAINT `evento_ibfk_1` FOREIGN KEY (`usuario`) REFERENCES `usuario` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
